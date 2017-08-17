73d34a7402cb89461682e81c2a9b7d1b90b28a73
Alonso <Alonso@PC-Alo>
2017-08-16 21:04:32 -0500

    Subido con las anotaciones y con Javadoc (una duda, ¿como se saca el javadoc y se sube a github?)

---
+++ src/edu/ipn/cecyt9/calculadora/CalculadoraMain.java
@@ -0,0 +1,27 @@
+
+package edu.ipn.cecyt9.calculadora;
+
+/**
+ * Ejercicio: Calculadora cientifica
+ * Implementar la Interfaz de usuario 
+ * Implementar los metodos siguientes a través de los listeners adecuados: 
+ * - Sumar 
+ * - Restar 
+ * - Multiplicar 
+ * - Dividir
+ * - Raiz cuadrada
+ * - Angulo seno
+ * - Angulo coseno
+ * - Angulo tangente
+ * - Elevar al cuadrado
+ * - Elevar a la x
+ *@author:  emmanuel 
+ * @version:  1.0 
+ * 
+ */
+public class CalculadoraMain {
+ public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}   
+}
