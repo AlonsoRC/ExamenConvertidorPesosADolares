15a8e4b596532ea215b85bfa6881378317c7e044
AlonsoRC <30801218+AlonsoRC@users.noreply.github.com>
2017-09-14 08:39:31 -0500

    Update Convertidor.java

--- src/Convertidor/Convertidor.java
+++ src/Convertidor/Convertidor.java
@@ -14,14 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
-/**
- * 
- * Interfaz para nuestra calculadora cientifica
- * 
- * @author:  Alonso 
- * @version:  1.0 
- * 
- */
+
 public class Convertidor extends JFrame {
 
 	
